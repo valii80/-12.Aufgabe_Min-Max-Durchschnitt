3ec6e6de60c05794b6594ac1dddec40b6f2e828c
valii80 <valii80.tm@gmail.com>
2026-02-02 10:27:45 +0100

    Min-Max-Durchschnitt

---
+++ 12.Aufgabe_Min-Max-Durchschnitt.git/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12.Aufgabe_Min_Max_Durchschnitt.git
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //12
+        }
+    }
+}
