ce3430265e231dfb31fc56986cf7022a13dce014
valii80 <valii80.tm@gmail.com>
2026-02-02 10:32:40 +0100

    Min-Max-Durchschnitt

--- 12.Aufgabe_Min-Max-Durchschnitt.git/Program.cs
+++ 12.Aufgabe_Min-Max-Durchschnitt.git/Program.cs
@@ -10,7 +10,30 @@ namespace _12.Aufgabe_Min_Max_Durchschnitt.git
     {
         static void Main(string[] args)
         {
-            //12
+            {
+                int min, max, summe = 0;
+
+                Console.Write("Zahl 1: ");
+                int zahl = int.Parse(Console.ReadLine());
+                min = max = zahl;
+                summe += zahl;
+
+                for (int i = 2; i <= 5; i++)
+                {
+                    Console.Write($"Zahl {i}: ");
+                    zahl = int.Parse(Console.ReadLine());
+
+                    if (zahl < min) min = zahl;
+                    if (zahl > max) max = zahl;
+
+                    summe += zahl;
+                }
+
+                double durchschnitt = summe / 5.0;
+
+                Console.WriteLine($"Min: {min}, Max: {max}, Durchschnitt: {durchschnitt}");
+            }
         }
+
     }
 }
